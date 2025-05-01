7bdcd7cb16f44bed33288a8dc3d79a5041005f00
socratest3 <socratest3@nycstudents.net>
2025-05-01 12:08:31 -0400

    Update Level.java

--- Level.java
+++ Level.java
@@ -1,1 +1,21 @@
-
+public class Level
+{
+    private boolean goalReached;
+    private int points;
+    public boolean goalReached()
+    {
+        return goalReached;
+    }
+    public int getPoints()
+    {
+        return points;
+    }
+    public void setPoints(int p)
+    {
+    points = p;
+    }
+    public void reachGoal()
+    {
+        goalReached=true;
+    }
+}
